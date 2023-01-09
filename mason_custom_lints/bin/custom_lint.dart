ea10975707a7432ab4676c416d8fc5015f609414
marcotrumpet <35219231+marcotrumpet@users.noreply.github.com>
2023-01-09 15:17:57 +0100

    1.0.0+3 (#4)
    * resolved dependencies and version update
    
    * updated readme

--- mason_custom_lints/bin/custom_lint.dart
+++ mason_custom_lints/bin/custom_lint.dart
@@ -4,8 +4,6 @@ import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
-import '../src/name_checkers/service_name_checker.dart';
-
 import '../src/name_checkers/bloc_name_checker.dart';
 
 void main(List<String> args, SendPort sendPort) {
@@ -20,11 +18,6 @@ class _MasonCustomLints extends PluginBase {
     final classes = library.topLevelElements.whereType<ClassElement>().toList();
 
     for (final classInstance in classes) {
-      yield* serviceShouldMatchFileName(
-        unit: unit,
-        classInstance: classInstance,
-      );
-
       yield* blocShouldMatchFileName(
         unit: unit,
         classInstance: classInstance,
